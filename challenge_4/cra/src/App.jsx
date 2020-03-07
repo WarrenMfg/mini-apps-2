965902cab8be6e8ca59c175173f97e454b25240c
WarrenMfg <warrenmfg@icloud.com>
2020-03-06 23:00:17 -0500

    conducted more research on redux and react-redux

--- challenge_4/cra/src/App.jsx
+++ challenge_4/cra/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Rows from './Rows.jsx';
 import './App.css';
+import { connect } from 'react-redux';
 
 class App extends React.Component {
   constructor(props) {
@@ -27,29 +28,39 @@ class App extends React.Component {
   }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    // add to props
+  };
+};
+
+export default connect()(App);
 
 
 /*
+watch this video: https://learn-2.galvanize.com/cohorts/1610/blocks/88/content_files/Redux/intro-to-redux.md
+
 const { createStore } = Redux;
 
 const initState = {
-  matrix: makeMatrix()
+  matrix: makeMatrix() // pass in this.props.rows???
 };
 
-cosnt reducer = (state = initState, action) => {
+cosnt reducer = (state = initState, action) => { // returns new state
 
 }
 
 const store = createStore( // reference to reducer function goes here // );
 
-const clickAction = { type: 'SQUARE_CLICKED' };
+const clickAction = { type: 'SQUARE_CLICKED' }; // must have type property
 
 // when user clicks, add this to callback
-store.dispatch(clickAction);
+store.dispatch(clickAction); // event emitter?
 
 // when state changes, do this
 store.subscribe(() => {
 
 })
+
+App --> dispatch an action --> reducer --> store
 */
---
+++ challenge_4/cra/src/reducers/rootReducer.js
@@ -0,0 +1,9 @@
+const initState = {
+  // matrix
+};
+
+const rootReducer = (state = initState, action) => {
+  return state; // temporary
+};
+
+export default rootReducer;
